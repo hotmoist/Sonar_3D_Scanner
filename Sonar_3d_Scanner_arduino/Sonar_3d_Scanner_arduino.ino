d8ed10b124612e05703e98f76f9c1d93c422f236
hj-k66 <hjx66@naver.com>
2020-08-02 00:40:51 +0900

    change condition of moving motor2, adding count variable

--- Sonar_3d_Scanner_arduino/Sonar_3d_Scanner_arduino.ino
+++ Sonar_3d_Scanner_arduino/Sonar_3d_Scanner_arduino.ino
@@ -1,5 +1,5 @@
 #include <AFMotor.h>
-
+#include <math.h>
 
 AF_Stepper stepper1(200, 1);
 AF_Stepper stepper2(200, 2);
@@ -10,7 +10,7 @@ int trigPin = 13;
 float angle = 0;
 float distance = 0;
 float height = 0;
-
+unsigned long count = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -34,18 +34,19 @@ void loop() {
   float distance = ((float)(340 * duration) / 10000) / 2;
 
   Serial.println(distance);
-  Serial.println((angle));
+  Serial.println(angle);
   Serial.println(height);
   
   // move table 1.8 degree
   stepper1.step(1, FORWARD, SINGLE);
   angle += 1.8;
-
-  if (fmod(angle, (float)360) == 0) {
+  count++;
+  
+  if(count%200 == 0){ //360 = 1.8*200
     stepper2.step(1, BACKWARD, SINGLE);
     height += 1.8;
   }
-  
+ 
   delay(1000);
 
 }
