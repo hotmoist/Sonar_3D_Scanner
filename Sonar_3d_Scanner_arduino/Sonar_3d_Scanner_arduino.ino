f2eb0c11d018d964505723b79a421fccb6a70984
hj-k66 <hjx66@naver.com>
2020-08-02 17:17:06 +0900

    exception and add end condition

--- Sonar_3d_Scanner_arduino/Sonar_3d_Scanner_arduino.ino
+++ Sonar_3d_Scanner_arduino/Sonar_3d_Scanner_arduino.ino
@@ -10,6 +10,8 @@ float angle = 0;
 float distance = 0;
 float height = 0;
 unsigned long count = 0;
+int TF = 0;
+int flag = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -21,6 +23,7 @@ void setup() {
 }
 
 void loop() {
+
   digitalWrite(trigPin, LOW);
   digitalWrite(echoPin, LOW);
   delayMicroseconds(2);
@@ -31,21 +34,37 @@ void loop() {
   unsigned long duration = pulseIn(echoPin, HIGH);
 
   float distance = ((float)(340 * duration) / 10000) / 2;
+  
+  flag++;
+  if (flag > 2) {
+    Serial.println(distance);
+    Serial.println(angle);
+    Serial.println(height);
+    Serial.println(TF);
 
-  Serial.println(distance);
-  Serial.println(angle);
-  Serial.println(height);
-  
-  // move table 1.8 degree
-  stepper1.step(1, FORWARD, SINGLE);
-  angle += 1.8;
-  count++;
-  
-  if(count%200 == 0){ //360 = 1.8*200
-    stepper2.step(1, BACKWARD, SINGLE);
-    height += 1.8;
+    // move table 1.8 degree
+    stepper1.step(1, FORWARD, SINGLE);
+    angle += 1.8;
+    count++;
+
+    if (distance < 50) {
+      TF++;
+    }
+
+    if (count % 200 == 0) { //360 = 1.8*200
+      if (TF > 0) {
+        stepper2.step(1, BACKWARD, SINGLE);
+        height += 0.07;
+      }
+      else {
+        //Serial.println("end");
+        Serial.println(0); //거리
+        Serial.println(0); //각도
+        Serial.println(1000);//높이
+        while (true);
+      }
+    }
   }
- 
-  delay(1000);
+  delay(250);
 
 }
